d0d45c23175dba8ddc0548bb6d303205b894cdb4
vtotiv <vito@dierksen.me>
2022-12-01 15:33:30 +0100

    even shorter with SplitWhen

--- day01.hs
+++ day01.hs
@@ -1,10 +1,12 @@
 import System.IO
 import Control.Monad
 import Data.List
+import Data.List.Split
 
 main = do
 	input <- readFile "01_input.txt"
 	-- part 1
-	print (maximum (map (sum . (map read) . (delete "")) (groupBy (\x y -> not (null y)) (lines input))))
+	print $ maximum $ map (sum . (map read)) (splitWhen null (lines input))
+
 	-- part 2
-	print (sum (take 3 (sortBy (flip compare) (map (sum . (map read) . (delete "")) (groupBy (\x y -> not (null y)) (lines input))))))
+	print $ sum $ take 3 (sortBy (flip compare) (map (sum . (map read)) (splitWhen null (lines input))))
