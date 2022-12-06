9bbbe11a7b8739d2cdbc3320e9307745671be517
vtotiv <vito@dierksen.me>
2022-12-06 11:54:04 +0100

    Day 6 shortened

--- 06_solution.hs
+++ 06_solution.hs
@@ -6,14 +6,14 @@ main = do
 	input <- readFile "06_input.txt"
 
 	-- part 1
-	print $ (+4) $ length $ takeWhile (not . isUnique) $ perms 4 $ head $ lines input
+	print $ (+4) $ length $ takeWhile (not . isUnique) $ perms 4 input
 
 	-- part 2
-	print $ (+14) $ length $ takeWhile (not . isUnique) $ perms 14 $ head $ lines input
+	print $ (+14) $ length $ takeWhile (not . isUnique) $ perms 14 input
 
 
 perms :: Int -> String -> [String]
-perms n s = filter (\x -> length x == n) (map (\x -> take n (drop x s)) [0..(length s - n)])
+perms n s = filter ((==n) . length) $ map (\x -> take n (drop x s)) [0..(length s - n)]
 
 isUnique :: String -> Bool
 isUnique s = length s == length (nub s)
